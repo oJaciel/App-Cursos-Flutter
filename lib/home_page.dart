302a037bb003907b03de83bb3896b7e85a4f5423
default <sememail@gmail.com>
2024-08-12 20:56:16 -0300

    Criação da Listagem de Dados

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,13 +8,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<String>> _futureCourses;
+
+  Future<List<String>> getCourses() async {
+    //Obter a lista da API
+    return Future.delayed(Duration(seconds: 10), () {
+      return ['Curso 1', 'Curso 2', 'Curso 3'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _futureCourses = getCourses();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Lista de Cursos"),
       ),
-      body: Text("Listagem de cursos vem aqui..."),
+      body: FutureBuilder(
+        future: _futureCourses,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index]),
+                    subtitle: const Text('Subtítulo vem aqui'),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    leading: const CircleAvatar(
+                      child: Text('CC'),
+                    ),
+                  );
+                });
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
