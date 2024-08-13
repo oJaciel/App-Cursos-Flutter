a03a31cca2e91847c6717f25c2fb0e5234d9e10e
default <sememail@gmail.com>
2024-08-12 22:16:04 -0300

    Ajuste de Repository

--- lib/model/course_repository.dart
+++ lib/model/course_repository.dart
@@ -1,10 +1,11 @@
 import 'package:appmobile/core/constants.dart';
+import 'package:appmobile/model/course_model.dart';
 import 'package:http/http.dart' as http;
 
 class courseRepository {
   final Uri url = Uri.parse('$urlBaseApi/course');
 
-  getAll() {
+  Future<List<courseEntity>> getAll() {
     final response = http.get(url);
   }
 }
