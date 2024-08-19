dde1ccffa72949d26bf25523e9cf3fa376ea73ad
default <sememail@gmail.com>
2024-08-19 20:08:58 -0300

    Finalização do Repository

--- lib/model/course_repository.dart
+++ lib/model/course_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:appmobile/core/constants.dart';
 import 'package:appmobile/model/course_model.dart';
 import 'package:http/http.dart' as http;
@@ -5,7 +7,19 @@ import 'package:http/http.dart' as http;
 class courseRepository {
   final Uri url = Uri.parse('$urlBaseApi/course');
 
-  Future<List<courseEntity>> getAll() {
-    final response = http.get(url);
+  //Buscando a lista de todos os cursos na API
+  Future<List<courseEntity>> getAll() async {
+    List<courseEntity> courseList = [];
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      //Convertendo JSON para lista
+      final json = jsonDecode(response.body) as List;
+
+      for (var course in json) {
+        courseList.add(courseEntity.fromJson(course));
+      }
+    }
+    return courseList;
   }
 }
