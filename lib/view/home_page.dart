6b70fcb89d7e1766ab709975b7e8bf5362489de5
oJaciel <jacielhbarichellO@gmail.com>
2024-08-31 23:05:37 -0300

    Aplicação de Botões Slidable nos Cursos + Ajuste Layout

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,6 +2,7 @@ import 'package:appmobile/controller/course_controller.dart';
 import 'package:appmobile/model/course_model.dart';
 import 'package:appmobile/view/form_new_course_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -52,23 +53,47 @@ class _HomePageState extends State<HomePage> {
         future: _futureCourses,
         builder: (context, AsyncSnapshot<List<CourseEntity>> snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  var course = snapshot.data![index];
-                  //Variável chamando a função do controller para montar avatar
-                  var avatarText = controller.getCourseAvatar(course);
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    var course = snapshot.data![index];
+                    //Variável chamando a função do controller para montar avatar
+                    var avatarText = controller.getCourseAvatar(course);
 
-                  return ListTile(
-                    title: Text(snapshot.data![index].name ?? "Não informado"),
-                    subtitle: Text(
-                        snapshot.data![index].description ?? "Não informado"),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    leading:
-                        CircleAvatar(child: Text(avatarText) //Montando Avatar,
-                            ),
-                  );
-                });
+                    return Card(
+                      elevation: 5,
+                      child: Slidable(
+                        endActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                                backgroundColor: Colors.grey,
+                                foregroundColor: Colors.white,
+                                icon: Icons.edit,
+                                onPressed: (context) {}),
+                            SlidableAction(
+                                backgroundColor: Colors.red,
+                                foregroundColor: Colors.white,
+                                icon: Icons.delete,
+                                onPressed: (context) {})
+                          ],
+                        ),
+                        child: ListTile(
+                          title: Text(
+                              snapshot.data![index].name ?? "Não informado"),
+                          subtitle: Text(snapshot.data![index].description ??
+                              "Não informado"),
+                          trailing: const Icon(Icons.arrow_forward_ios),
+                          leading: CircleAvatar(
+                              child: Text(avatarText) //Montando Avatar,
+                              ),
+                        ),
+                      ),
+                    );
+                  }),
+            );
           } else {
             return const Center(child: CircularProgressIndicator());
           }
